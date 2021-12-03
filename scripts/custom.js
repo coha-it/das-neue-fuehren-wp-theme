75db6b9fa7012464d89930ca68afa089db2622b9
Das Neue Fuehren <posfuehr312521@corporate-happiness.de>
2021-12-03 13:24:11 +0100

    Timer added

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,54 +7,53 @@ var dnf = {
                 window.location.hash.includes('admin')
                 ) {
                 return false;
-        } else {
-            window.location.href=url
-        }
-    })
+            } else {
+                window.location.href=url
+            }
+        })
+    },
+
+    calcTime: function() {
+        var now = new Date().getTime();
+        var countDownDate = new Date(jQuery('[countdown]').attr('countdown')).getTime();
+        var timeleft = countDownDate - now;
+
+        if (timeleft > 0) {
+                // Timer
+                var days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+                var hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+                var msg = '';
+                msg += days      ? days    + ' Tage ' : '';
+                msg += hours     ? hours   + ' Stunden ' : '';
+                msg += minutes   ? minutes + ' Minuten ' : '';
+                msg += seconds + ' Sekunden ';
+                msg += '';
+
+
+                jQuery('[countdown]').html(msg);
+                jQuery('[countdown-running]').show();
+                jQuery('[countdown-finished]').hide();
+
+                setTimeout(function() {
+                    dnf.calcTime();
+                }, 1000);
+            } else {
+                // Timer Over!
+                // console.log('Finished!');
+                // Hide Countdown and Show Livestream
+                jQuery('[countdown-running]').hide();
+                jQuery('[countdown-finished]').show();
+            }
     }
 }
 
 window.dnf = dnf;
 
-
-function calcTime() {
-    var now = new Date().getTime();
-    var countDownDate = new Date(jQuery('[countdown]').attr('countdown')).getTime();
-    var timeleft = countDownDate - now;
-
-    if (timeleft > 0) {
-            // Timer
-            var days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-            var hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-            var msg = '';
-            msg += days      ? days    + ' Tage ' : '';
-            msg += hours     ? hours   + ' Stunden ' : '';
-            msg += minutes   ? minutes + ' Minuten ' : '';
-            msg += seconds + ' Sekunden ';
-            msg += '';
-
-
-            jQuery('[countdown]').html(msg);
-            jQuery('[countdown-running]').show();
-            jQuery('[countdown-finished]').hide();
-
-            setTimeout(function() {
-                calcTime();
-            }, 1000);
-        } else {
-            // Timer Over!
-            // console.log('Finished!');
-            // Hide Countdown and Show Livestream
-            jQuery('[countdown-running]').hide();
-            jQuery('[countdown-finished]').show();
-        }
-
-
+jQuery( document ).ready(function() {
+    if (dnf) {
+        dnf.calcTime();
     }
-
-    jQuery( document ).ready(function() {
-        calcTime();
-    });
+});
